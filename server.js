a7b736ad7e73bf3a1d562661d7577efa8d049c04
SergeySapr <sergey.activate@gmail.com>
2016-09-02 14:05:58 +0000

    Introduced a default message

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var express = require('express');
+const express = require('express');
+const dateformat = require("dateformat");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:str',function(req,res){
+ var str = req.params.str.toString();
+ var naturalDate = new Date(str);
+ if (isNaN(naturalDate.valueOf())) {
+     naturalDate = new Date(str*1000)
+     
+ }
+ console.log(str,naturalDate);
+ //if is natural date
+ if (!isNaN(naturalDate.valueOf())) {
+    res.send("'unix':"+naturalDate.valueOf()/1000+",'natural':"+dateformat(naturalDate,"dd mmm yyyy"))
+ } else res.send("This is not a date. Provide either a natural date value (eg 01Jan2016) or a unix timestamp (eg  1451606400).");
+ 
+});
+
+app.get('/',function(req,res){
+ res.send("Provide either a natural date value (eg 01Jan2016) or a unix timestamp (eg  1451606400).");
 });
 
 app.listen(8080, function () {
